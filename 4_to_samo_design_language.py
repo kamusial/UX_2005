80eb2cc052d7eeac4a73ea16b5f5b90887cbaedf
student-wroclaw <student-wroclaw@EDU-WSB.local>
2023-05-20 11:37:31 +0200

    1

--- 4_to_samo_design_language.py
+++ 4_to_samo_design_language.py
@@ -5,10 +5,14 @@ from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
+from kivy.uix.widget import Widget
+
+class MyGrid(Widget):
+    pass
 
 class MyApp(App):
     def build(self):
-        return Label(text='Hello')
+        return MyGrid()
 
 if __name__ == '__main__':
     MyApp().run()
