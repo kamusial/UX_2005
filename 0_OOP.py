dcd32aaf0bac2a98f425e7213e4d99876bc477cf
student-wroclaw <student-wroclaw@EDU-WSB.local>
2023-05-20 09:12:21 +0200

    1

---
+++ 0_OOP.py
@@ -0,0 +1,30 @@
+class Auto:
+    def __init__(self, brand, rocznik, barwa):
+        self.marka = brand
+        self.kolor = barwa
+        self.wiek = 2023 - rocznik
+        self.kondycja = 5
+        self.ilosc_paliwa = 10
+        self.tryb_ekonomiczny = False
+        self.spalanie_na_100 = 14
+        self.przebieg = 0
+
+    def zasieg(self):
+        _zasieg = self.ilosc_paliwa / self.spalanie_na_100 * 100 * 0.9
+        return round(_zasieg)
+
+    def ustaw_tryb(self, tryb):
+        if tryb == 'eco':
+            self.spalanie_na_100 = 10
+            self.tryb_ekonomiczny = True
+            print('Tryb eco')
+        elif tryb == 'normal':
+            self.spalanie_na_100 = 14
+            self.tryb_ekonomiczny = False
+            print('Tryb normal')
+        else:
+            print('tryb nierozpoznany, brak zmian')
+
+    moje_auto =
+
+
