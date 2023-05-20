4ce142bbfc87cdc7caff562ea0cb6247b2584c94
student-wroclaw <student-wroclaw@EDU-WSB.local>
2023-05-20 10:41:23 +0200

    1

--- 3_grid_button_event.py
+++ 3_grid_button_event.py
@@ -10,22 +10,26 @@ class MyGrid(GridLayout):
     def __init__(self, **kwargs):
         super(MyGrid, self).__init__(**kwargs)
 
-        self.cols = 2
+        self.cols = 1
 
-        self.add_widget(Label(text='Imie'))
+        self.inside = GridLayout()
+        self.inside.cols = 2
+
+        self.inside.add_widget(Label(text='Imie'))
         self.imie = TextInput(multiline=False)
-        self.add_widget(self.imie)
+        self.inside.add_widget(self.imie)
 
-        self.add_widget(Label(text='Nazwisko'))
+        self.inside.add_widget(Label(text='Nazwisko'))
         self.nazwisko = TextInput(multiline=False)
-        self.add_widget(self.nazwisko)
+        self.inside.add_widget(self.nazwisko)
 
-        self.add_widget(Label(text='wiek'))
+        self.inside.add_widget(Label(text='wiek'))
         self.wiek = TextInput(multiline=False)
-        self.add_widget(self.wiek)
+        self.inside.add_widget(self.wiek)
 
+        self.add_widget(self.inside)
         self.add_widget(Button(text='slij', font_size=40))
-        self.add_widget(Button(text='slij', font_size=40))
+
 
 class MyApp(App):
     def build(self):
