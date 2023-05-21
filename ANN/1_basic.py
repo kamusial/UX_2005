6928002cd129edfb6f0e556acb634af1be6219e7
student-wroclaw <student-wroclaw@EDU-WSB.local>
2023-05-21 11:05:27 +0200

    1

--- ANN/1_basic.py
+++ ANN/1_basic.py
@@ -6,21 +6,33 @@ from tensorflow.keras.layers import Dense
 from tensorflow.random import set_seed
 import matplotlib.pyplot as plt
 set_seed(0)
+np.random.seed(0)
 
 #model
 model = Sequential()
-model.add(Dense(4, input_shape=[1], activation="linear"))
-model.add(Dense(2, activation="linear"))
+model.add(Dense(1, input_shape=[1], activation="linear"))
+model.add(Dense(4, activation="linear"))
+model.add(Dense(4, activation="linear"))
+model.add(Dense(4, activation="linear"))
 model.add(Dense(1))
 
 #kompilacja
-model.compile(optimizer="rmsprop", loss="mse")
+model.compile(optimizer="rmsprop", loss="mse", metrics=["mae"])
 
 df = pd.read_csv("f-c.csv", usecols=[1,2])
 
-plt.scatter(df.F, df.C)
+
+result = model.fit(df.F, df.C, epochs=500, verbose=1)
+print(result.history.keys())
+
+df1 = pd.DataFrame(result.history)
+print(df1.head())
+df1.plot()
 plt.show()
 
+C_pred = model.predict(df.F)
 
-
+plt.scatter(df.F, df.C)
+plt.plot(df.F, C_pred, c='r')
+plt.show()
 
