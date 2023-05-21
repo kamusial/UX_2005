7cad897bc824b32e1af2c10a87e56d99a0fb3bf3
student-wroclaw <student-wroclaw@EDU-WSB.local>
2023-05-21 10:13:30 +0200

    1

--- ANN/1_basic.py
+++ ANN/1_basic.py
@@ -4,11 +4,23 @@ import pandas as pd
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from tensorflow.random import set_seed
-
+import matplotlib.pyplot as plt
 set_seed(0)
 
+#model
 model = Sequential()
-model.add(Dense(4), input_shape=[1], activation="linear")
+model.add(Dense(4, input_shape=[1], activation="linear"))
 model.add(Dense(2, activation="linear"))
 model.add(Dense(1))
+
+#kompilacja
+model.compile(optimizer="rmsprop", loss="mse")
+
+df = pd.read_csv("f-c.csv", usecols=[1,2])
+
+plt.scatter(df.F, df.C)
+plt.show()
+
+
+
 
